45c94767a0db277fec72149811a87361b8f23957
bgoonz <bryan.guner@gmail.com>
2021-06-08 13:38:13 -0400

    --- Auto-Git Commit ---

---
+++ projects/LECTURE/experimental/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import { titleReducer } from './titleReducer';
+import { membersReducer } from './membersReducer';
+
+export default combineReducers({ title: titleReducer, members: membersReducer })
