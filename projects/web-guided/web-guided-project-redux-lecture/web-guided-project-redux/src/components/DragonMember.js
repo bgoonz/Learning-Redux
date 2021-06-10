28400bdb783f26f0fbbdecb91a8ff7b518bb53d6
bryan <bryan.guner@gmail.com>
2021-06-10 15:23:59 -0400

    update

---
+++ projects/web-guided/web-guided-project-redux-lecture/web-guided-project-redux/src/components/DragonMember.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DragonMember = (props)=> {
+    const { member } = props;
+
+    return(<h4>
+        {member.name}
+        {member.dragonStatus && <i className="fas fa-dragon" />}
+    </h4>);
+}
+
+export default DragonMember;
