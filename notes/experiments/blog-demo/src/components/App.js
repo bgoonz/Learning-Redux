96780ed15275f9cdca8c82c3be057e0c8a73b8ef
bryan <bryan.guner@gmail.com>
2021-06-10 12:08:02 -0400

    message

---
+++ notes/experiments/blog-demo/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Sidebar from './Sidebar'
+import Switcher from './Switcher'
+
+import styles from '../css/App'
+
+export default () =>
+  <div className={styles.app}>
+    <Sidebar />
+    <Switcher />
+  </div>
---
+++ notes/experiments/blog-demo/src/reducers/direction.js
@@ -0,0 +1,6 @@
+export default (state = 'next', action = {}) =>
+  action && action.meta && action.meta.location
+    && action.meta.location.kind === 'pop'
+    ? 'back'
+    : 'next'
+
